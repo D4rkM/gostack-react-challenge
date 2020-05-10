114666dcb594719120b6938a488cd77c98279ff8
Magno <magno.oliveira@luizalabs.com>
2020-05-09 22:04:05 -0300

    tests: change url

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -18,14 +18,14 @@ const actWait = async (amount = 0) => {
 };
 
 describe("App component", () => {
-  it("should be able to add new repository", async () => {
+  it("should be able to add new repository", async () => { 
     const { getByText, getByTestId } = render(<App />);
 
     apiMock.onGet("repositories").reply(200, []);
 
     apiMock.onPost("repositories").reply(200, {
       id: "123",
-      url: "https://github.com/josepholiveira",
+      url: "https://github.com/d4rkm",
       title: "Desafio ReactJS",
       techs: ["React", "Node.js"],
     });
@@ -47,7 +47,7 @@ describe("App component", () => {
     apiMock.onGet("repositories").reply(200, [
       {
         id: "123",
-        url: "https://github.com/josepholiveira",
+        url: "https://github.com/d4rkm",
         title: "Desafio ReactJS",
         techs: ["React", "Node.js"],
       },
